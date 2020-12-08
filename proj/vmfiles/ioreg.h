6f48369c302ce97284ae999694edcc1a957d82fa
vigno88 <nathan.alix88@gmail.com>
2020-12-07 19:48:10 -0500

    ioreg

--- proj/vmfiles/ioreg.h
+++ proj/vmfiles/ioreg.h
@@ -8,8 +8,9 @@
 #define __CmVM_IOReg_h
 
 #include "_stdtype.h"
+#include "_ioreg.h" 
 
-u32  IOReg_Read (u32 ioreg);
-void IOReg_Write(u32 ioreg, u32 value);
+u32  IOReg_Read (u32 ioreg){ return _IOReg_Read(u32 ioreg); }
+void IOReg_Write(u32 ioreg, u32 value) { _IOReg_Write(u32 ioreg, u32 value); }
 
 #endif
